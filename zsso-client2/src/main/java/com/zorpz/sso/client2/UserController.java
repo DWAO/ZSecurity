374f52b972daae87a0a7d80aa8fa420e0cd5a21d
DWAO <774797836@qq.com>
2019-05-11 18:37:28 +0800

    增加sso 简易版本

---
+++ zsso-client2/src/main/java/com/zorpz/sso/client2/UserController.java
@@ -0,0 +1,29 @@
+package com.zorpz.sso.client2;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Punk
+ * @date 2019/05/11
+ */
+@RestController
+public class UserController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @RequestMapping("/test1")
+    public String test1() {
+        return "test1";
+    }
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @RequestMapping("/test2")
+    public String test2() {
+        return "test2";
+    }
+}
